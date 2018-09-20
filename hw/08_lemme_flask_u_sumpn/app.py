5eddfe799e2bfeb8550e6e6f7bab563f7abb8aa5
SashaKor <you@example.com>
2018-09-19 22:24:40 -0400

    mydrop

---
+++ hw/08_lemme_flask_u_sumpn/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+app = Flask(__name__) #instantiates the Flask class using a constructor
+
+@app.route('/')
+def home():
+ return 'Atom is the best religion'
+
+from flask import Flask
+app = Flask(__name__) #instantiates the Flask class using a constructor
+
+@app.route('/')
+def home1():
+ return '<a href= /hello> oh no </a>'
+
+@app.route('/hello')
+def home2():
+ return '<a href= /hello/world> oh nah </a>'
+
+@app.route('/hello/world')
+def home3():
+ return 'ya"ll'
+  
+app.debug = True
+app.run()
