d4057f80839341a668c2f37c4b15a354c35b7b22
SashaKor <you@example.com>
2018-09-30 11:53:13 -0400

    exploration

--- 13_formation/app.py
+++ 13_formation/app.py
@@ -7,18 +7,18 @@ from flask import Flask,render_template,request
 app = Flask(__name__) #create instance of class Flask
 
 
-@app.route("/")
+@app.route("/",methods= ["GET","POST"])
 def hello_world(): #assign fxn to route
     print(app)
     return render_template('input.html')
 
-@app.route("/auth")
+@app.route("/auth",methods= ["GET","POST"])
 def authenticate() :
     print(app)
     print(request)
     print(request.args['username'])
     return render_template('output.html',
-                           username=request.args['username'],
+                           username=request.post('username')
                            method= request)
     
  
