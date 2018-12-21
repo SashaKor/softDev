10768795866728ed72eb3373f5a389f90897210a
SashaKor <akoroza@stuy.edu>
2018-12-20 19:33:52 -0500

    lol

--- 30/dommy.js
+++ 30/dommy.js
@@ -1,3 +1,18 @@
+//Team: ASCIIresort
+//<Frist> <Lsat>
+//SoftDev1 pd8
+//K #30: Sequential Progression III: Season of the Witch
+//2018-12-20
+
+
+/*
+PHASE III
+Upon button push, add an element to the list.
+When the mouse goes over an item in the list, change the heading at the top to contain the text of the item.
+When the mouse is no longer over an item in the list, change the heading back to "Hello World!".
+When an item on the list is clicked, remove it from the DOM.
+*/
+
 var but0 = document.getElementById("b");
 //console.log(fibbut);
 but0.addEventListener('click', function(x){
@@ -14,5 +29,12 @@ var lst = document.getElementById("thelist");
 //console.log(fibbut);
 lst.addEventListener('mouseover', function(e){
   console.log(e);
-  document.getElementById("h").innerHTML = "Paragraph changed!";
+  var str= lst.innerHTML;
+  document.getElementById("h").innerHTML = str;
+});
+
+lst.addEventListener('mouseout', function(e){
+  console.log(e);
+  //var str= lst.innerHTML;
+  document.getElementById("h").innerHTML = "Hello World";
 });
