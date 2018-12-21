319e23fc95a394194a43c9175e74823902e0ba18
Sasha <akoroza@stuy.edu>
2018-12-21 14:01:35 -0500

    done

--- 30_js/dommy.js
+++ 30_js/dommy.js
@@ -13,48 +13,35 @@ When the mouse is no longer over an item in the list, change the heading back to
 When an item on the list is clicked, remove it from the DOM.
 */
 
+var but0 = document.getElementById("b"); //button for adding items to the first list
+var children = document.getElementById("thelist").children; //gets the terms in the first list
+
+var update = function(){
+  for (var i=0; i<children.length; i++){ //loops through all the terms of the first list
+    children[i].addEventListener('mouseover', function(){
+      document.getElementById("h").innerHTML = this.innerHTML; //puts the innerHTML of whats in the term into the heading
+    });
+    children[i].addEventListener('mouseout', function(){
+      document.getElementById("h").innerHTML = "Hello World"; //puts the default value if the mouse moves off the list
+    });
+    children[i].addEventListener('click', function(){this.remove();} // click to remove a list
+  );};
+};
+
+update() //activates the mouse over functionality before button is pressed. NECESSARY FOR IT TO WORK
+
 /*adding element to list upon button push*/
-var but0 = document.getElementById("b");
-var lst = document.getElementById("thelist");
-var children= lst.children;
-//var lst = document.getElementsByTagName("li");
-//console.log(fibbut);
-but0.addEventListener('click', function(x){
-  console.log(x);
+but0.addEventListener('click', function(){ //looks for a button click
   var elem = document.createElement("LI");
   //used to append some text to a newly created li element
   var text = document.createTextNode("NEWNODE!!!");
   elem.appendChild(text);
-  document.getElementById("thelist").appendChild(elem);
-  var lst = document.getElementById("thelist");
-  var children= lst.children
-  for (var i=0; i<children.length; i++){
-    children[i].addEventListener('mouseover', function(e){
-      console.log(e);
-      document.getElementById("h").innerHTML = this.innerHTML;
-    });
-    children[i].addEventListener('mouseout', function(){
-      document.getElementById("h").innerHTML = "Hello World";
-    });
-    children[i].addEventListener('click', function(){this.remove();}
-  );
-  };
+
+  document.getElementById("thelist").appendChild(elem); //puts the new term into the list
+
+  update(); //updates the mouseover functionality
   }
-
 );
-
-for (var i=0; i<children.length; i++){
-  children[i].addEventListener('mouseover', function(e){
-    console.log(e);
-    document.getElementById("h").innerHTML = this.innerHTML;
-  });
-  children[i].addEventListener('mouseout', function(){
-    document.getElementById("h").innerHTML = "Hello World";
-  });
-  children[i].addEventListener('click', function(){this.remove();}
-);
-};
-
 
 /*
 PHASE IV:
@@ -64,24 +51,21 @@ When the second button is pressed,
 add a new item to your list, showing the next Fibonacci number.
 Beyond!: add more lists that generate other sequences…
 */
-var fibNum = 1;
 
-var fib = function(n){
-    if (n == 0)
-        return 0;
-    else if (n<3)
-        return 1;
-    else
-        return fib(n -1) + fib(n -2);
-};
+var fibNum = 1; // numbers for the fib
+var fibBefore = 0;
 
-var but1 = document.getElementById("fb");
+var but1 = document.getElementById("fb"); //button for the fib function
 but1.addEventListener('click', function(e){
-  console.log(e);
-  var elem = document.createElement("LI");
-  //used to append some text to a newly created list element
-  var text = document.createTextNode(fib(fibNum++));
+  var elem = document.createElement("li");
+
+  var text = document.createTextNode(fibNum); //creates the next term of fib
   elem.appendChild(text);
-  document.getElementById("fiblist").appendChild(elem);
+
+  document.getElementById("fiblist").appendChild(elem); //adds the new term
+
+  var holdNum = fibNum; //updates fibNum
+  fibNum+=fibBefore;
+  fibBefore = holdNum;
 }
 );
